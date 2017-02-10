8ab6e0524d939deaf14ae26c8e68ff3d55503830
bennypaulino <santiagopro71@gmail.com>
2017-02-10 01:31:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetRadius::Application.config.secret_token = '162b3fee0ff12ad6b81df1a3b0b7d9b27e60ec3ed87e5998e3838b3f9c2ccc964fde891344f3eabc65daf96f6bf5622920efe556b806a54e9131a8f84401abfd'
